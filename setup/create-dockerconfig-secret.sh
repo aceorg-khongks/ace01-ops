91526fcb95b21788f7227f3de9d536cba4839c2c
Kok Sing Khong <kskhong@au1.ibm.com>
2023-08-19 13:59:02 +1000

    Test - don't use dockerconfig-secret

--- setup/create-dockerconfig-secret.sh
+++ setup/create-dockerconfig-secret.sh
@@ -3,22 +3,31 @@
 # need to export IBM_ENTITLEMENT_KEY
 NAMESPACE=${1:-ace01-ci}
 
-IMAGE_REGISTRY_SERVICE_URL=image-registry.openshift-image-registry.svc.cluster.local:5000
-IMAGE_REGISTRY_SERVICE_AUTH_B64=$(echo $(oc whoami):$(oc whoami -t) | base64)
+IMAGE_REGISTRY_SERVICE_AUTH_B64=$(echo "kubeadmin:$(oc whoami -t)" | base64)
 IBM_ENTITLEMENT_AUTH_B64=$(echo cp:${IBM_ENTITLEMENT_KEY} | base64)
 
-cat <<EOF > config.json
-{
-  "auths" : {
-    "${IMAGE_REGISTRY_SERVICE_URL}" : {
-      "auth": ${IMAGE_REGISTRY_SERVICE_AUTH_B64}
-    },
-    "cp.icr.io" : {
-      "auth": ${IBM_ENTITLEMENT_AUTH_B64}
-    }
-  }
-}
-EOF
+echo ${IMAGE_REGISTRY_SERVICE_AUTH_B64}
+echo ${IBM_ENTITLEMENT_AUTH_B64}
+
+# cat <<EOF | oc apply -f -
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: dockerconfig-secret
+#   namespace: ${NAMESPACE}
+# stringData:
+#   config.json: |
+#     {
+#       "auths" : {
+#         "image-registry.openshift-image-registry.svc.cluster.local:5000" : {
+#           "auth": "${IMAGE_REGISTRY_SERVICE_AUTH_B64}"
+#         },
+#         "cp.icr.io" : {
+#           "auth": "${IBM_ENTITLEMENT_AUTH_B64}"
+#         }
+#       }
+#     }
+# EOF
 
 cat <<EOF | oc apply -f -
 apiVersion: v1
@@ -26,6 +35,13 @@ kind: Secret
 metadata:
   name: dockerconfig-secret
   namespace: ${NAMESPACE}
-data:
-  config.json: $(cat config.json | base64)
+stringData:
+  config.json: |
+    {
+      "auths" : {
+        "image-registry.openshift-image-registry.svc.cluster.local:5000" : {
+          "auth": "${IMAGE_REGISTRY_SERVICE_AUTH_B64}"
+        }
+      }
+    }
 EOF
