1b2a79f9def9327b0eb1acf1f9aefacb166fe7c7
Kok Sing Khong <kskhong@au1.ibm.com>
2023-08-19 22:01:18 +1000

    Adde debug in buildah.yaml

--- setup/create-dockerconfig-secret.sh
+++ setup/create-dockerconfig-secret.sh
@@ -9,26 +9,6 @@ IBM_ENTITLEMENT_AUTH_B64=$(echo cp:${IBM_ENTITLEMENT_KEY} | base64)
 echo ${IMAGE_REGISTRY_SERVICE_AUTH_B64}
 echo ${IBM_ENTITLEMENT_AUTH_B64}
 
-# cat <<EOF | oc apply -f -
-# apiVersion: v1
-# kind: Secret
-# metadata:
-#   name: dockerconfig-secret
-#   namespace: ${NAMESPACE}
-# stringData:
-#   config.json: |
-#     {
-#       "auths" : {
-#         "image-registry.openshift-image-registry.svc.cluster.local:5000" : {
-#           "auth": "${IMAGE_REGISTRY_SERVICE_AUTH_B64}"
-#         },
-#         "cp.icr.io" : {
-#           "auth": "${IBM_ENTITLEMENT_AUTH_B64}"
-#         }
-#       }
-#     }
-# EOF
-
 cat <<EOF | oc apply -f -
 apiVersion: v1
 kind: Secret
@@ -41,7 +21,27 @@ stringData:
       "auths" : {
         "image-registry.openshift-image-registry.svc.cluster.local:5000" : {
           "auth": "${IMAGE_REGISTRY_SERVICE_AUTH_B64}"
+        },
+        "cp.icr.io" : {
+          "auth": "${IBM_ENTITLEMENT_AUTH_B64}"
         }
       }
     }
 EOF
+
+# cat <<EOF | oc apply -f -
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: dockerconfig-secret
+#   namespace: ${NAMESPACE}
+# stringData:
+#   config.json: |
+#     {
+#       "auths" : {
+#         "image-registry.openshift-image-registry.svc.cluster.local:5000" : {
+#           "auth": "${IMAGE_REGISTRY_SERVICE_AUTH_B64}"
+#         }
+#       }
+#     }
+# EOF
